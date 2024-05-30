fd9e0a1fd9c26e64e5da79bd5c4f8da6cff473b3
孙文旭 <wenxu.sun@opple.com>
2024-05-30 15:37:28 +0800

    外部模型导入

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [
-    vue(),
-  ],
+  optimizeDeps: {
+    include: ['dat.gui']
+  },
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
